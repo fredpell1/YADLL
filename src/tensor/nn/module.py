63c83294441e01f1b40de26b2fcbd405c8beb23a
fredpell1 <fredericpelletier7@gmail.com>
2023-06-13 16:44:07 -0400

    more layers and sequential container

--- src/tensor/nn/module.py
+++ src/tensor/nn/module.py
@@ -1,13 +1,15 @@
+from src.tensor.autodiff import Tensor
+from src.tensor.nn.functional import Tensor
 from ..autodiff import *
 from .functional import *
-from typing import List, Generator
+from typing import Any, List, Generator
 
 class Module:
     
     def __init__(self) -> None:
         self.params : List[Tensor] = []
 
-    def parameters(self) -> Generator[Tensor, any, any]:
+    def parameters(self) -> Generator[Tensor, Any, Any]:
         for p in self.params:
             yield p
 
@@ -30,8 +32,98 @@ class Linear(Module):
 
     def forward(self,x: Tensor) -> Tensor:
         assert x.shape[-1] == self.in_features
-        output = x @ self.weight.transpose(0,1) + self.b if self.bias else x @ self.weight.T
+        output = x @ self.weight.T + self.b if self.bias else x @ self.weight.T
         return output 
 
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+
+class Sum(Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return x.sum()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+class Mean(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.mean()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+class Max(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.max()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+
+
+class ReLU(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.relu()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+    
+class Exp(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.exp()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+class Log(Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.log()
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
+
+
 class Sequential(Module):
-    pass
+    
+    def __init__(self, *args) -> None:
+        super().__init__()
+        for arg in args:
+            assert isinstance(arg, Module)
+            self.params.append(arg)
+    
+    def parameters(self) -> Generator[Tensor, Any, Any]:
+        for module in self.params:
+            for param in module.parameters():
+                yield param
+
+    def append(self, module : Module) -> None:
+        self.params.append(module)
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = x
+        for layer in self.params:
+            out = layer(out)
+        return out    
+    
+    def __call__(self, x: Tensor) -> Tensor:
+        return self.forward(x)
--- test/test_tensor/test_tensor_module.py
+++ test/test_tensor/test_tensor_module.py
@@ -29,3 +29,44 @@ def test_linear_layer_backward():
         output.backward()
         torch_output.backward()
         assert np.all(layer.weight.grad == torch_layer.weight.grad.detach().numpy())
+
+def test_sequential_output():
+    with torch.no_grad():
+        x = Tensor(np.array([1.0,2,3,4]).reshape(1,-1), requires_grad=True)
+        net = Sequential(
+            Linear(4,10),
+            ReLU(), 
+            Linear(10,1)
+        )
+        torch_x = torch.tensor([1.0,2,3,4], dtype=torch.float64).reshape(1,-1)
+        torch_net = torch.nn.Sequential(
+             torch.nn.Linear(4,10, dtype=torch.float64),
+             torch.nn.ReLU(),
+             torch.nn.Linear(10,1, dtype=torch.float64)
+        )
+        for param, torch_param in zip(net.parameters(), torch_net.parameters()):
+            torch_param.data.copy_(torch.tensor(param.data.reshape(torch_param.data.shape), dtype=torch.float64))
+
+        assert abs(net(x).data[0,0] - torch_net(torch_x).numpy()[0,0]) < 0.00000001
+
+def test_sequential_backward():
+    x = Tensor(np.array([1.0,2,3,4]).reshape(1,-1), requires_grad=True)
+    net = Sequential(
+        Linear(4,10),
+        ReLU(), 
+        Linear(10,1)
+    )
+    torch_x = torch.tensor([1.0,2,3,4], dtype=torch.float64).reshape(1,-1)
+    torch_net = torch.nn.Sequential(
+            torch.nn.Linear(4,10, dtype=torch.float64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(10,1, dtype=torch.float64)
+    )
+    for param, torch_param in zip(net.parameters(), torch_net.parameters()):
+        torch_param.data.copy_(torch.tensor(param.data.reshape(torch_param.data.shape), dtype=torch.float64))
+    out = net(x)
+    torch_out = torch_net(torch_x)
+    out.backward()
+    torch_out.backward()
+    for param, torch_param in zip(net.parameters(), torch_net.parameters()):
+        assert np.all(abs(param.grad - torch_param.grad.detach().numpy()) < 0.0000001)
