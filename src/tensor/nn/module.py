fbebc3feb5886843ea898a05872d8bb20ed2b179
fredpell1 <fredericpelletier7@gmail.com>
2023-06-20 14:09:29 -0400

    conv2d!

--- src/tensor/nn/module.py
+++ src/tensor/nn/module.py
@@ -4,21 +4,20 @@ from ..autodiff import *
 from .functional import *
 from typing import Any, List, Generator
 
+
 class Module:
-    
     def __init__(self) -> None:
-        self.params : List[Tensor] = []
+        self.params: List[Tensor] = []
 
     def parameters(self) -> Generator[Tensor, Any, Any]:
         for p in self.params:
             yield p
 
-    def forward(self, x:Tensor, *args, **kwargs) -> Tensor:
+    def forward(self, x: Tensor, *args, **kwargs) -> Tensor:
         raise NotImplementedError("You should override this method in a subclass")
 
 
 class Linear(Module):
-    
     def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
         super().__init__()
         self.in_features = in_features
@@ -27,27 +26,80 @@ class Linear(Module):
         self.weight = Tensor.random((out_features, in_features), True)
         self.params.append(self.weight)
         if bias:
-            self.b = Tensor.random((1,out_features), True)
+            self.b = Tensor.random((1, out_features), True)
             self.params.append(self.b)
 
-    def forward(self,x: Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> Tensor:
         assert x.shape[-1] == self.in_features
         output = x @ self.weight.T + self.b if self.bias else x @ self.weight.T
-        return output 
+        return output
 
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
+
+
+class Conv2d(Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: tuple,
+        stride: tuple=(1,1),
+        padding: tuple[tuple]=((0,0), (0,0)),
+        bias=True,
+    ) -> None:
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.bias = bias
+        self.weight = Tensor.random((out_channels, in_channels, kernel_size[0],kernel_size[1]), True, 'weight')
+        self.params.append(self.weight)
+        if bias:
+            self.b = Tensor.random((out_channels,), True, 'bias')
+            self.params.append(self.b)
+
+    def __compute_out_dim(self, Hin, Win) -> tuple[int, int]:
+        assert self.padding[0][0] == self.padding[0][1]
+        assert self.padding[1][0] == self.padding[1][1]
+        Hout = np.floor(
+            (Hin + 2*self.padding[0][0] - self.kernel_size[0])/self.stride[0] + 1
+        )
+        Wout = np.floor(
+            (Win + 2*self.padding[1][0] - self.kernel_size[1])/self.stride[1] + 1
+        )
+        return int(Hout), int(Wout)
+
+    def forward(self, x: Tensor) -> Tensor:
+        padded_x = x.pad(((0,0),(0,0),*self.padding)) if any(pad > 0 for tup in self.padding for pad in tup) else x
+        Hout, Wout = self.__compute_out_dim(x.shape[-2], x.shape[-1]) #could optimize and only compute this once
+        Hin, Win = padded_x.shape[-2], padded_x.shape[-1]
+        out = Tensor.zeros((x.shape[0], self.out_channels, Hout, Wout), name="out")
+        for batch in range(padded_x.shape[0]):
+            for out_index in range(self.out_channels):
+                for in_index  in range(self.in_channels):
+                    for i,h_kernel in enumerate(range(0, (Hin - self.kernel_size[0])+ 1, self.stride[0])):
+                        for j,w_kernel in enumerate(range(0, (Win - self.kernel_size[1]) + 1, self.stride[1])):
+                            count += 6
+                            out[batch,out_index,i,j] += (self.weight[out_index, in_index, :, :] * padded_x[batch, in_index, h_kernel:h_kernel+self.kernel_size[0], w_kernel:w_kernel + self.kernel_size[1]]).sum()
+
+                if self.bias:
+                    out[batch, out_index,:, :] += self.b[out_index] #adding the bias out of in_channels loop to avoid over-adding it if multiple channels
+        return out
 
 
 class Sum(Module):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def forward(self, x: Tensor) -> Tensor:
         return x.sum()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
+
 
 class Mean(Module):
     def __init__(self) -> None:
@@ -55,9 +107,10 @@ class Mean(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return x.mean()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
+
 
 class Max(Module):
     def __init__(self) -> None:
@@ -65,10 +118,9 @@ class Max(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return x.max()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
-
 
 
 class ReLU(Module):
@@ -77,53 +129,53 @@ class ReLU(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return x.relu()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
-    
+
+
 class Exp(Module):
     def __init__(self) -> None:
         super().__init__()
 
     def forward(self, x: Tensor) -> Tensor:
         return x.exp()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
 
-class Log(Module):
 
+class Log(Module):
     def __init__(self) -> None:
         super().__init__()
 
     def forward(self, x: Tensor) -> Tensor:
         return x.log()
-    
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
 
 
 class Sequential(Module):
-    
     def __init__(self, *args) -> None:
         super().__init__()
         for arg in args:
             assert isinstance(arg, Module)
             self.params.append(arg)
-    
+
     def parameters(self) -> Generator[Tensor, Any, Any]:
         for module in self.params:
             for param in module.parameters():
                 yield param
 
-    def append(self, module : Module) -> None:
+    def append(self, module: Module) -> None:
         self.params.append(module)
 
     def forward(self, x: Tensor) -> Tensor:
         out = x
         for layer in self.params:
             out = layer(out)
-        return out    
-    
+        return out
+
     def __call__(self, x: Tensor) -> Tensor:
         return self.forward(x)
--- test/test_tensor/test_tensor_module.py
+++ test/test_tensor/test_tensor_module.py
@@ -1,6 +1,9 @@
 from src.tensor.nn.module import *
 import torch
 import numpy as np
+import traceback
+
+#TODO: test linear layer on batch input
 
 def test_linear_layer_output():
     with torch.no_grad():
@@ -70,3 +73,87 @@ def test_sequential_backward():
     torch_out.backward()
     for param, torch_param in zip(net.parameters(), torch_net.parameters()):
         assert np.all(abs(param.grad - torch_param.grad.detach().numpy()) < 0.0000001)
+
+
+def test_conv2d_output_default_stride():
+    with torch.no_grad():
+        x = Tensor.random((1, 1, 3,3), True)
+        conv = Conv2d(1,1,(2,2),stride=(1,1),padding = ((1,1), (1,1)))
+        torch_x = torch.tensor(x.data, dtype=torch.float64)
+        torch_conv = torch.nn.Conv2d(1,1,2,padding=(1,1))
+        torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+        torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+        out = conv.forward(x)
+        torch_out = torch_conv(torch_x)
+        assert np.all(abs(out.data - torch_out.numpy()) < 0.00000001)
+
+def test_conv2d_output_two_stride():
+    with torch.no_grad():
+        x = Tensor.random((1, 1, 3,3), True)
+        conv = Conv2d(1,1,(2,2),stride=(2,2),padding = ((1,1), (1,1)))
+        torch_x = torch.tensor(x.data, dtype=torch.float64)
+        torch_conv = torch.nn.Conv2d(1,1,2,stride=(2,2),padding=(1,1))
+        torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+        torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+        out = conv.forward(x)
+        torch_out = torch_conv(torch_x)
+        assert np.all(abs(out.data - torch_out.numpy()) < 0.00000001)
+
+def test_conv2d_output_batched_input():
+    x = Tensor.random((2, 1, 3,3), True)
+    conv = Conv2d(1,1,(2,2),stride=(1,1),padding = ((1,1), (1,1)))
+    torch_x = torch.tensor(x.data, dtype=torch.float64)
+    torch_conv = torch.nn.Conv2d(1,1,2,padding=(1,1))
+    torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+    torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+    out = conv.forward(x)
+    torch_out = torch_conv(torch_x)
+    assert np.all(abs(out.data - torch_out.detach().numpy()) < 0.00000001)
+
+def test_conv2d_output_multiple_in_channels():
+    x = Tensor.random((1, 3, 3,3), True)
+    conv = Conv2d(3,1,(2,2),stride=(1,1),padding = ((1,1), (1,1)))
+    torch_x = torch.tensor(x.data, dtype=torch.float64)
+    torch_conv = torch.nn.Conv2d(3,1,2,padding=(1,1))
+    torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+    torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+    out = conv.forward(x)
+    torch_out = torch_conv(torch_x)
+    assert np.all(abs(out.data - torch_out.detach().numpy()) < 0.00000001)
+    
+def test_conv2d_output_multiple_out_channels():
+    x = Tensor.random((1, 1, 3,3), True)
+    conv = Conv2d(1,3,(2,2),stride=(1,1),padding = ((1,1), (1,1)))
+    torch_x = torch.tensor(x.data, dtype=torch.float64)
+    torch_conv = torch.nn.Conv2d(1,3,2,padding=(1,1))
+    torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+    torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+    out = conv.forward(x)
+    torch_out = torch_conv(torch_x)
+    assert np.all(abs(out.data - torch_out.detach().numpy()) < 0.00000001)
+    
+def test_conv2d_no_bias_backward():
+    x = Tensor.random((1, 1, 3,3), True, name='x')
+    conv = Conv2d(1,1,(2,2),stride=(1,1),padding = ((0,0), (0,0)), bias=False)
+    torch_x = torch.tensor(x.data, dtype=torch.float64)
+    torch_conv = torch.nn.Conv2d(1,1,2,padding=(0,0), bias=False)
+    torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+    out = conv.forward(x).mean()
+    torch_out = torch_conv(torch_x).mean()
+    out.backward()
+    torch_out.backward()
+    assert np.all(abs(conv.weight.grad - torch_conv.weight.grad.detach().numpy()) < 0.00000001)
+
+def test_conv2d_with_bias_backward():
+    x = Tensor.random((1, 1, 3,3), True, name='x')
+    conv = Conv2d(1,1,(2,2),stride=(1,1),padding = ((0,0), (0,0)), bias=True)
+    torch_x = torch.tensor(x.data, dtype=torch.float64)
+    torch_conv = torch.nn.Conv2d(1,1,2,padding=(0,0), bias=True)
+    torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
+    torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
+    out = conv.forward(x).mean() * 3.2 - 2.3
+    torch_out = torch_conv(torch_x).mean() * 3.2 -2.3
+    out.backward()
+    torch_out.backward()
+    assert np.all(abs(conv.weight.grad - torch_conv.weight.grad.detach().numpy()) < 0.00000001)
+    assert np.all(abs(conv.b.grad - torch_conv.bias.grad.detach().numpy()) < 0.00000001)
