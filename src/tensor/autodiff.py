fix add: now works with broadcasting

src/tensor/autodiff.py
@@ -1,6 +1,28 @@
 from __future__ import annotations
 from typing import Union, Tuple
 import numpy as np
+
+def add_dimensions(old_shape, new_shape):
+    if len(old_shape) <= len(new_shape):
+        minimal_length = old_shape
+        other_shape = new_shape
+    else:
+        minimal_length = new_shape
+        other_shape = old_shape
+    
+    return minimal_length + (1,) * (len(other_shape) - len(minimal_length))
+
+def shape_to_axis(old_shape,new_shape):
+    # taken from https://github.com/geohot/tinygrad/blob/master/tinygrad/runtime/ops_cpu.py
+    if len(old_shape) == 0 or len(new_shape) == 0: return None
+    if len(old_shape) < len(new_shape):
+        old_shape = add_dimensions(old_shape, new_shape)
+    elif len(old_shape) > len(new_shape):
+        new_shape = add_dimensions(old_shape, new_shape)
+    size = tuple(i for i,(a,b) in enumerate(zip(old_shape, new_shape)) if a != b)
+    print(size)
+    return size
+
 class Tensor():
 
     def __init__(self, data: np.array, requires_grad: bool = False, parent = (), op='', name='') -> None:
@@ -66,7 +88,8 @@ class Tensor():
         def _backward():
             self.grad += output.grad
             if other.requires_grad:
-                other.grad += output.grad if other.shape == output.shape else output.grad.sum()
+                other.grad += output.grad if other.shape == output.shape else \
+                    output.grad.sum(axis=shape_to_axis(self.shape, other.shape), keepdims=True).reshape(other.grad.shape)
         output._backward = _backward
         return output
     

test/test_tensor/test_tensor_autodiff.py
@@ -23,6 +23,7 @@ def test_setitem_backward_pass():
                   [7,8,9]]), requires_grad=True)
     y = Tensor.zeros(x.shape, True)
     y[0,0] = x[0,0]
+    y = y**2
     y[0,1] = x[1,1]
     z = y.mean()
     z.backward()
@@ -31,6 +32,7 @@ def test_setitem_backward_pass():
                   [7,8,9]], dtype=torch.float64, requires_grad=True)
     torch_y = torch.zeros_like(torch_x)
     torch_y[0,0] = torch_x[0,0]
+    torch_y = torch_y ** 2
     torch_y[0,1] = torch_x[1,1]
     torch_z = torch_y.mean()
     torch_x.retain_grad()
@@ -49,17 +51,62 @@ def test_add_scalar_backward_pass():
     torch_y.backward()
     assert np.all(b.grad == torch_b.grad.detach().numpy())
 
+def test_add_tensor_same_shape_backward_pass():
+    x = Tensor(np.array([[1.0,2], [2,3]]), requires_grad=True)
+    y = Tensor(np.array([[1.0,1], [3.2, 4.3]]), requires_grad=True)
+    z = (x + y).sum() ** 2
+    z.backward()
+    
+    torch_x = torch.tensor([[1.0,2],[2,3]], requires_grad=True)
+    torch_y = torch.tensor([[1.0,1], [3.2, 4.3]], requires_grad=True)
+    torch_x.retain_grad()
+    torch_y.retain_grad()
+    torch_z = (torch_x + torch_y).sum() ** 2
+    torch_z.backward()
+    assert np.all(x.grad == torch_x.grad.detach().numpy())
+    assert np.all(y.grad == torch_y.grad.detach().numpy())
+
+def test_add_tensor_row_broadcast_backward_pass():
+    x = Tensor(np.array([[1.0,2], [2,3]]), requires_grad=True)
+    y = Tensor(np.array([1.0,1]), requires_grad=True)
+    z = (x + y).sum() ** 2
+    z.backward()
+    
+    torch_x = torch.tensor([[1.0,2],[2,3]], requires_grad=True)
+    torch_y = torch.tensor([1.0,1], requires_grad=True)
+    torch_x.retain_grad()
+    torch_y.retain_grad()
+    torch_z = (torch_x + torch_y).sum() ** 2
+    torch_z.backward()
+    assert np.all(x.grad == torch_x.grad.detach().numpy())
+    assert np.all(y.grad == torch_y.grad.detach().numpy())
+
+def test_add_tensor_col_broadcast_backward_pass():
+    x = Tensor(np.array([[1.0,2], [2,3]]), requires_grad=True)
+    y = Tensor(np.array([[1.0],[1]]), requires_grad=True)
+    z = (x + y).sum() ** 2
+    z.backward()
+    
+    torch_x = torch.tensor([[1.0,2],[2,3]], requires_grad=True)
+    torch_y = torch.tensor([[1.0],[1]], requires_grad=True)
+    torch_x.retain_grad()
+    torch_y.retain_grad()
+    torch_z = (torch_x + torch_y).sum() ** 2
+    torch_z.backward()
+    assert np.all(x.grad == torch_x.grad.detach().numpy())
+    assert np.all(y.grad == torch_y.grad.detach().numpy())
+
+
 def test_mul_scalar_backward_pass():
     a = Tensor(np.array([[1,2],[2,1]]), requires_grad = True)
-    b = a * 2
+    b = (a * 2).sum() ** 2
     b.backward()
     c = torch.tensor([[1.0,2],[2,1]], requires_grad=True)
-    d = (c * 2)
-    d.backward(torch.ones_like(d)) #need to pass a tensor because d has more than one element
-    print(a.grad, c.grad)
+    d = (c * 2).sum() ** 2
+    d.backward() #need to pass a tensor because d has more than one element
     assert np.all(a.grad == c.grad.numpy())
     
-def test_mul_matrix_backward_pass():
+def test_mul_tensor_same_shape_backward_pass():
     a = Tensor(np.array([[1,2],[2,1]]), requires_grad = True)
     b = Tensor(np.array([[2,2],[3,4]]), requires_grad = True)
     c = a * b

test/test_tensor/test_tensor_module.py
@@ -148,7 +148,7 @@ def test_conv2d_with_bias_backward():
     x = Tensor.random((1, 1, 3,3), True, name='x')
     conv = Conv2d(1,1,(2,2),stride=(1,1),padding = ((0,0), (0,0)), bias=True)
     torch_x = torch.tensor(x.data, dtype=torch.float64)
-    torch_conv = torch.nn.Conv2d(1,1,2,padding=(0,0), bias=True)
+    torch_conv = torch.nn.Conv2d(1,1,3,padding=(0,0), bias=True)
     torch_conv.weight = torch.nn.Parameter(torch.tensor(conv.weight.data))
     torch_conv.bias = torch.nn.Parameter(torch.tensor(conv.b.data))
     out = conv.forward(x).mean() * 3.2 - 2.3