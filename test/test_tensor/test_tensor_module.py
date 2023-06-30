fix matmul with broadcasting for real

test/test_tensor/test_tensor_module.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import traceback
 
-#TODO: test linear layer on batch input
+
 
 def test_linear_layer_output():
     with torch.no_grad():
@@ -33,6 +33,19 @@ def test_linear_layer_backward():
         torch_output.backward()
         assert np.all(layer.weight.grad == torch_layer.weight.grad.detach().numpy())
 
+def test_multidim_linear_layer_backward():
+    x = Tensor.random((4,3,10))
+    layer = Linear(10,10)
+    torch_x = torch.tensor(x.data, requires_grad=True)
+    torch_layer = torch.nn.Linear(10,10, dtype=torch.float64)
+    torch_layer.weight = torch.nn.Parameter(torch.tensor(layer.weight.data, dtype=torch.float64))
+    torch_layer.bias = torch.nn.Parameter(torch.tensor(layer.b.data, dtype=torch.float64))
+    output = layer.forward(x).relu().sum()
+    torch_output = torch_layer(torch_x).relu().sum()
+    output.backward()
+    torch_output.backward()
+    assert np.all(abs(layer.weight.grad - torch_layer.weight.grad.detach().numpy()) < 0.00000001)
+        
 def test_sequential_output():
     with torch.no_grad():
         x = Tensor(np.array([1.0,2,3,4]).reshape(1,-1), requires_grad=True)