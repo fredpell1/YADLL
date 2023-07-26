9a93e12db4b9853c41600d638db1b12ef78395e9
fredpell1 <fredericpelletier7@gmail.com>
2023-07-26 15:35:24 -0400

    setup.py first try

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="yadll",
+    description="Yet Another Deep Learning Library",
+    version="0.1",
+    author="Frederic Pelletier",
+    license="MIT",
+    packages=['src', 'src.nn'],
+    install_requires=['numpy', 'scikit-image'],
+    python_requires=">=3.9",
+    extras_require={
+        'testing': ['torch', 'pytest']
+    },
+    include_package_data=True
+)
