837228cf84f17518022794ed37bf76a8a0ee57a6
fredpell1 <fredericpelletier7@gmail.com>
2023-08-04 14:08:24 -0400

    optimizer base class

--- test/test_optimizers/test_sgd.py
+++ test/test_optimizers/test_sgd.py
@@ -1,4 +1,8 @@
 from yadll.tensor.autodiff import *
+from yadll.tensor.optimizers import *
+from yadll.tensor.nn import *
 import torch
 
 # TODO Add some unit-tests for stochastic gradient descent.
+def test():
+    assert 0
---
+++ yadll/tensor/optimizers/optimizer.py
@@ -0,0 +1,21 @@
+class Optimizer():
+    def __init__(self, params) -> None:
+        self.params = params
+
+    def step(self):
+        raise NotImplementedError("You should override this method in a subclass")
+    
+    def zero_grad(self):
+        for p in self.params:
+            p.grad = None
+
+class SGD(Optimizer):
+    def __init__(self, params, lr: float, momentum: float = 0) -> None:
+        super().__init__(params)
+        self.lr = lr
+        self.momentum = momentum
+
+    def step(self):
+        #TODO: implement step for SGD
+        for p in self.params:
+            continue
