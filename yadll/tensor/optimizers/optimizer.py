f002c041d747b1949562fb2574d1960c439a9284
Transurgeon <peter.zijie@gmail.com>
2023-08-06 14:59:30 -0400

    adding base implementation

--- yadll/tensor/optimizers/optimizer.py
+++ yadll/tensor/optimizers/optimizer.py
@@ -1,9 +1,14 @@
-class Optimizer:
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class Optimizer(ABC):
     def __init__(self, params) -> None:
         self.params = params
 
+    @abstractmethod
     def step(self):
-        raise NotImplementedError("You should override this method in a subclass")
+        pass
 
     def zero_grad(self):
         for p in self.params:
@@ -15,8 +20,17 @@ class SGD(Optimizer):
         super().__init__(params)
         self.lr = lr
         self.momentum = momentum
+        self.velocities = [np.zeros_like(p) for p in self.params]
 
     def step(self):
-        # TODO: implement step for SGD
-        for p in self.params:
-            continue
+        # Implement step for SGD with momentum
+        for i, p in enumerate(self.params):
+            if p.grad is not None:
+                if self.momentum > 0:
+                    # Update velocity
+                    self.velocities[i] = self.momentum * self.velocities[i] + (1 - self.momentum) * p.grad
+                    # Apply momentum in the update
+                    p.data -= self.lr * self.velocities[i]
+                else:
+                    # Basic SGD update without momentum
+                    p.data -= self.lr * p.grad
