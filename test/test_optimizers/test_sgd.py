968738e500bab3106ba2af540e9dab5d0e32d868
fredpell1 <fredericpelletier7@gmail.com>
2023-08-20 16:23:48 -0400

    make test with momentum fail again

--- test/test_optimizers/test_sgd.py
+++ test/test_optimizers/test_sgd.py
@@ -32,11 +32,14 @@ def test_sgd_with_momentum():
     torch_model.weight = torch.nn.Parameter(torch.tensor(model.weight.data))
     optim = SGD(model.parameters(), 0.1, 0.9)
     torch_optim = torch.optim.SGD(torch_model.parameters(), 0.1, 0.9)
-    out = model(x).sum()
-    torch_out = torch_model(torch_x).sum()
-    out.backward()
-    torch_out.backward()
     assert np.all(model.weight.data == torch_model.weight.detach().numpy()), "weight not equal before step"
-    optim.step()
-    torch_optim.step()
+    for _ in range(3):
+        x = Tensor.random((2, 2))
+        torch_x = torch.tensor(x.data)
+        out = model(x).sum()
+        torch_out = torch_model(torch_x).sum()
+        out.backward()
+        torch_out.backward()
+        optim.step()
+        torch_optim.step()
     assert np.all(model.weight.data == torch_model.weight.detach().numpy()), "weight not equal after step"
